9e6d69ccc4cb185ca35c579d99fa0b0d0b773d34
Megan Renshaw <meganrenshaw19@gmail.com>
2014-09-22 11:44:42 -0500

    add user following

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,13 @@ DemoApp::Application.routes.draw do
   match '/signout', to: 'sessions#destroy',     via: 'delete'
   
   resources :microposts, only: [:create, :destroy]
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :sessions, only: [:new, :create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
